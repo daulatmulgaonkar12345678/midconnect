b56f47d2ae0cdeaee367bc26845e09d3481410dd
Daulat Mulgaonkar <daulatmulgaonkar@gmail.com>
2026-02-22 09:28:43 +0000

    auto-commit for 807a2f3c-68c8-4179-9b4c-b589456f6251

---
+++ frontend/.next/dev/static/chunks/src_app_layout_tsx_1cddbe20._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__5d5b4b86._.css",
+  "static/chunks/src_lib_cloudinary_ts_6e679734._.js",
+  "static/chunks/src_a57b58ca._.js",
+  "static/chunks/node_modules_029fe993._.js"
+],
+    source: "dynamic"
+});
---
+++ frontend/.next/dev/static/chunks/src_lib_cloudinary_ts_cc232e06._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_lib_cloudinary_ts_36b294ab._.js"
+],
+    source: "dynamic"
+});
