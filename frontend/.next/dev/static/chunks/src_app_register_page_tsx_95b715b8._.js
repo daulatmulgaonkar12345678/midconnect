f9d8bb4294d45a4d77827b87b1cf2368ab8aa695
Daulat Mulgaonkar <daulatmulgaonkar@gmail.com>
2026-02-22 09:32:53 +0000

    Remove Next.js build artifacts and cache from repository

--- frontend/.next/dev/static/chunks/src_app_register_page_tsx_95b715b8._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_6b66a471._.js"
-],
-    source: "dynamic"
-});
