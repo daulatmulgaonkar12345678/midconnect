c4d5c3d981a79abf70b22422aff1c3772b6cd7b0
Daulat Mulgaonkar <daulatmulgaonkar@gmail.com>
2026-02-22 09:29:49 +0000

    auto-commit for 514482e2-d5aa-4cba-93a3-fa7af2106fdc

--- frontend/.next/dev/static/chunks/src_app_register_page_tsx_157eb91a._.js
+++ frontend/.next/dev/static/chunks/src_app_register_page_tsx_157eb91a._.js
@@ -1,10 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__5d5b4b86._.css",
-  "static/chunks/src_lib_cloudinary_ts_f6e80ffa._.js",
-  "static/chunks/src_c39bf7b7._.js",
-  "static/chunks/node_modules_029fe993._.js"
+  "static/chunks/_6b66a471._.js"
 ],
     source: "dynamic"
 });
