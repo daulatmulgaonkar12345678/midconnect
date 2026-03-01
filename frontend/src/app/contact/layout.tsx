00291b900e3fdb46b7e8ec0fe188177845ccf2da
Daulat Mulgaonkar <daulatmulgaonkar@gmail.com>
2026-03-01 08:06:25 +0000

    auto-commit for a1defd4b-d864-4ec6-ada9-d011becc8a6f

---
+++ frontend/src/app/contact/layout.tsx
@@ -0,0 +1,6 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Contact Us - UdyogConnect',
+  description: 'Get in touch with UdyogConnect support team. We are here to help with your queries about buying, selling, or using our platform.',
+};
